3d3f4ef914e8e420f98a073a042b690874b15739
unknown <cjramey@gmail.com>
2014-12-12 22:13:04 -0600

    Adding index file and app assets (#1)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/app.css">
+
+	<title>Classroom</title>
+</head>
+<body>
+	
+
+	<script type="text/javascript" src="assets/vendor/jquery/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/app.js"></script>
+</body>
+</html>
